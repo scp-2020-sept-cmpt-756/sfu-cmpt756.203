8f4f793c654db238ec5fec639b9944d6ce82768d
George Chow <george@overcoil.com>
2021-02-26 21:23:41 -0800

    syntax error

--- e-k8s/tools/prep-csil.sh
+++ e-k8s/tools/prep-csil.sh
@@ -6,6 +6,7 @@
 # Gatling container) and crucially, sets the permissions, to allow the container's Scala assets to be built/saved
 
 for c in resources results target target/test-classes target/test-classes/proj756 target/test-classes/computerdatabase
+do
   mkdir gatling/${c}
   chmod 777 gatling/${c}
 done
