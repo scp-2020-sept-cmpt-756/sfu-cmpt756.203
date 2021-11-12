0518b7097baa4e42f5e2eae95df2fc4e113acca2
Ted Kirkpatrick <ted@sfu.ca>
2021-11-12 10:02:32 -0800

    Add script to start tool container

---
+++ e-k8s/tools/shell.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Start the CMPT 756 environment
+REGISTRY=ghcr.io
+USERID=tedkirkpatrick
+VER=v1.0
+docker container run -it --rm \
+  -v ${HOME}/.aws:/root/.aws \
+  -v ${HOME}/.azure:/root/.azure \
+  -v ${HOME}/.ssh:/root/.ssh \
+  -v ${HOME}/.kube:/root/.kube \
+  -v ${HOME}/.config:/root/.config \
+  -v ${PWD}/gatling/results:/opt/gatling/results \
+  -v ${PWD}/gatling:/opt/gatling/user-files \
+  -v ${PWD}/gatling/target:/opt/gatling/target \
+  -v ${PWD}:/home/k8s \
+  ${REGISTRY}/${USERID}/cmpt-756-tools:${VER}
